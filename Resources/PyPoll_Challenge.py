b99340c9343f48d4c45c510fb878d1f01f853b9d
Daniel Gandia <gandiadaniel@gmail.com>
2021-03-10 18:38:47 -0500

    Updating the Challenge Code

--- Resources/PyPoll_Challenge.py
+++ Resources/PyPoll_Challenge.py
@@ -73,7 +73,7 @@ with open(file_to_save, "w") as txt_file:
         county_percentage = float(voter_turnout) / float(total_votes) * 100
         # 6d: Print the county results to the terminal.
         county_results = (
-        f"{county_name}: {county_percentage:1f}% ({voter_turnout:,})\n")
+        f"{county_name}: {county_percentage:.1f}% ({voter_turnout:,})\n")
         print(county_results)
         # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
